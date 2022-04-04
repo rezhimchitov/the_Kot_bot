7f501a53233e7ff077cd293e12f21e4f019d2c8d
rezhimchitov <yakovleva.evelina.vit@gmail.com>
2022-04-05 00:14:37 +0300

    Taro_cards done 78 with pic

--- card.py
+++ card.py
@@ -1,5 +1,6 @@
 class GenericCard:
     def __init__(self, card_name, card_description, card_picture):
+        self.link_prefix = 'taro_pictures\\'
         self.name = card_name
         self.description = card_description
-        self.picture = card_picture
+        self.picture = self.link_prefix + card_picture + '.jpg'
